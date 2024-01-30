a088a112a904f6f5240ecaf8bf392f51521cc5ed
kewei5zhang <keweizhang411@gmail.com>
2024-01-31 09:58:38 +1100

    simulate failed deployments

--- js/packages/frontend/index.js
+++ js/packages/frontend/index.js
@@ -1,6 +1,6 @@
 // https://medium.com/@mohammedijas/hello-world-in-node-js-b333275ddc89
 const http = require('http');
-const hostname = 'localhost';
+const hostname = '0.0.0.0';
 const port = 3000;
 const server = http.createServer((req, res) => {
  console.log(req.headers);
